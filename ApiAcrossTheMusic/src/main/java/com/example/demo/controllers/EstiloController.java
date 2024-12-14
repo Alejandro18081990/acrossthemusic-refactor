05199bdbb04110a816f49663c9680b87921e6d8b
manuel.nunez <alexnrdriguez@gmail.com>
2024-12-14 10:15:53 +0100

    Feat: añado configuración a application.properties para integrar seguridad con Keycloak

--- ApiAcrossTheMusic/src/main/java/com/example/demo/controllers/EstiloController.java
+++ ApiAcrossTheMusic/src/main/java/com/example/demo/controllers/EstiloController.java
@@ -3,14 +3,11 @@ package com.example.demo.controllers;
 
 import com.common_microservicios.commons.controller.CommonController;
 import com.example.demo.dto.estiloDTO.EstiloDTO;
-import com.example.demo.dto.musicoDto.MusicoDTO;
 import com.example.demo.entities.Estilo;
-
-
 import com.example.demo.services.EstiloService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
