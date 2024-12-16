fd6f0411bfedd14730d960e0437e7faf646c5ef3
Alejandro18081990 <alexnrdriguez@gmail.com>
2024-12-16 17:30:37 +0100

    Feat: añado configuraciones necesarias a application.properties para que el proyecto disponga del protoclo https y la seguridad esté mediada por Keycloak. Se añade la clase SecurityConfig que gestiona la seguridad de la api y KeycloakRoleConverter que se encarga de transformar los roles recibidos en el access token.

--- ApiAcrossTheMusic/src/main/java/com/example/demo/configuracion/SecurityConfig.java
+++ ApiAcrossTheMusic/src/main/java/com/example/demo/configuracion/SecurityConfig.java
@@ -1,9 +1,43 @@
 package com.example.demo.configuracion;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        http
+                .csrf(csrf -> csrf.disable())
+                //  Establece el protocolo https por defecto
+                .requiresChannel(rc ->
+                        rc.anyRequest().requiresSecure())
+                // Restringe el acceso a todos los endpoints del proyecto requiriendo autenticación
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2ResourceServer(rsc ->
+                        rsc.jwt(jwt ->
+                                jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("https://localhost:8443/realms/acrossthemusic/protocol/openid-connect/certs")
+                .build();
+    }
+
+
 }
---
+++ ApiAcrossTheMusic/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("apiATM/usuarios")
+@Tag(name = "PI: Across the music - Controlador usuarios")
+public class UserController {
+
+
+}
